2f4e9ccc20a49f5cb025d31e971b4047e624de45
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-07-31 21:17:03 +0000

    jogando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 int main(){
 
-   printf("Desafio das cartas!\n");
+   int Carta;
+   char Estado[50];
+   char Codigo[10];
+   char cidade[50];
+   int Populacao;
+   float Area;
+   float Pib;
+   int turisticos;
+
+   printf("Carta:\n");
+   scanf("%d" ,&Carta);
+
+   printf("Estado:\n");
+   scanf("%s",&Estado);
+
+   printf("Codigo:\n");
+   scanf("%f",&Codigo);
+
+   printf("Cidade:\n");
+   scanf("%s", &Cidade);
+
+   printf("Populacao:\n");
+   scanf("%d", %Populacao);
+   
+
 }
