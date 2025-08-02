45ac73b1e0a5f9056cee06305dff9dd47935321c
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-02 21:26:53 +0000

    jogango 3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,16 +3,37 @@ int main(){
 
    int Carta1;
    char Estado1[50];
-   char Codigo1[10];
+   char Codigo1[05];
    char Cidade1[50];
    int Populacao1;
    float Area1;
    float Pib1;
    int pontosTuristicos1;
 
-   printf("Digite o nome da cidade para a Carta 1: ");
-   scanf("%s[^\\n]" ,&Cidade1);
+   printf("Digite o nome da cidade da Carta 1:\n");
+   scanf("%s" ,Cidade1);
 
-   printf("")
+   printf("Digite o Estado da carta 1:\n");
+   scanf("%s" ,Estado1);
+
+   printf("Digite a populacao da carta 1:\n");
+   scanf("%d" ,&Populacao1);   
+    
+   printf("Digite o Codigo da carta 1:\n");
+   scanf("%s" ,Codigo1);
+
+   printf("Digite a area da carta 1:\n");
+   scanf("%f" ,&Area1);
+
+   printf("Digite o Pib da carta 1:\n");
+   scanf("%f" ,&Pib1);
+
+   printf("Digite pontos Turisticos da carta 1:\n");
+   scanf("%d" ,&pontosTuristicos1);
+
+
+   return 0;
+
+   
 }
    
