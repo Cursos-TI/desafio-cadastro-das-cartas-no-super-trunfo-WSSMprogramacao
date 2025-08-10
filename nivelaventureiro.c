5bc2a971c12169631933ec2d323fe03d378c8b44
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-10 02:56:23 +0000

    operacoes 1

---
+++ nivelaventureiro.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+    /*
+    soma (+)
+    subtracao (-)
+    multiplicacao (*)
+    divisao (/)
+    */
+
+    int numero1 + numero2;
+    int soma,subtracao,multiplicacao,divisao;
+    // operacao soma
+    soma = numero1 + numero2;
+    // operacao subtracao
+    subtracao = numero1 - numero2;
+    // operacao multiplicacao
+    multiplicacao = numero1 * numero2;
+    // operacao divisao
+    divisao = numero1 / numero2;
+
+
+
+}
