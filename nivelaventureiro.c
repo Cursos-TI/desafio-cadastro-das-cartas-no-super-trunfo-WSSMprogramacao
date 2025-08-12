8766bb96ca3d55a44895fa65455854cc14a5ceea
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-12 22:01:27 +0000

    proc.ari 1

--- nivelaventureiro.c
+++ nivelaventureiro.c
@@ -8,9 +8,15 @@ int main(){
     divisao (/)
     */
 
-    int numero1 + numero2;
+    int numero1,numero2;
     int soma,subtracao,multiplicacao,divisao;
-    // operacao soma
+
+    printf("Entre com numero 1: \n");
+    scanf("%d", &numero1);
+    printf("Entre com numero 2: \n");
+    scanf("%d", &numero2);
+
+    // operacao soma 
     soma = numero1 + numero2;
     // operacao subtracao
     subtracao = numero1 - numero2;
@@ -18,6 +24,11 @@ int main(){
     multiplicacao = numero1 * numero2;
     // operacao divisao
     divisao = numero1 / numero2;
+
+    printf("A soma é: %d\n",soma);
+    printf("A subtração é: %d\n",subtracao);
+    printf("A multiplicação é: %d\n",multiplicacao);
+    printf("A Divisão é: %d\n",divisao);
 
 
 
