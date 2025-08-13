5cf2d4a9608812ccbd855dc3d5508f13e67087ab
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-13 21:51:49 +0000

    incremento ....

---
+++ increm.decremento.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+Incremento (++)
+Pre-incremento ++a
+Pos-incremento a++
+Decremento (--)
+Pre-decremento --a
+Pos-incremento a--
+*/
+
+int numero1= 1;
+
+printf("Antes incremento: %d\n", numero1);
+//numero1= numero 1 + 1;
+//numero1 += 1;
+numero1++;
+
+printf("Apos incremento: %d\n", numero1);
+//numero1= numero 1 - 1;
+//numero1 -= 1;
+numero1--;
+
+printf(" Apos decremento: %d\n", numero1);
+
+
+
+}
+
+
