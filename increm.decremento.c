8ea11a372fff225cc9f48812929123b958492ed6
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-15 20:44:15 +0000

    increm 1

--- increm.decremento.c
+++ increm.decremento.c
@@ -11,19 +11,33 @@ Pre-decremento --a
 Pos-incremento a--
 */
 
-int numero1= 1;
+int numero1= 1, resultado;
 
 printf("Antes incremento: %d\n", numero1);
 //numero1= numero 1 + 1;
 //numero1 += 1;
-numero1++;
 
-printf("Apos incremento: %d\n", numero1);
+//pos-incremento;
+//resultado = numero1;
+//numero1 = ++;
+resultado = numero1++;
+//printf("Apos incremento: %d\n", numero1);
+printf("Apos pos-incremento - numero1: %d, resultado %d\n", numero1, resultado);
+
+resultado = ++numero1;
+printf("Apos pre-incremento - numero1: %d, resultado %d\n", numero1, resultado);
+
+resultado = numero1--;
+printf("Apos pos-decremento - numero1: %d, resultado %d\n", numero1, resultado);
+
+resultado = --numero1;
+printf("Apos pre-decremento - numero1: %d, resultado %d\n", numero1, resultado);
+
+
 //numero1= numero 1 - 1;
 //numero1 -= 1;
-numero1--;
+//numero1--;
 
-printf(" Apos decremento: %d\n", numero1);
 
 
 
