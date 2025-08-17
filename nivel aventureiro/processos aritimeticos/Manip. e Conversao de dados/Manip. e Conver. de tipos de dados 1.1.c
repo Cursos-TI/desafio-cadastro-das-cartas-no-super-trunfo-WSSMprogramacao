ab209bbdee33d8b450ec22155867340b33811cd1
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-17 03:51:04 +0000

    prosseguir

---
+++ nivel aventureiro/processos aritimeticos/Manip. e Conversao de dados/Manip. e Conver. de tipos de dados 1.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 10;
+    int b = 3;
+    int soma = a + b;
+    int subtracao = a - b;
+    int produto = a * b;
+    int quociente = a / b;  //note que a divisao de inteiros resulta em um numero inteiro
+
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", subtracao);
+    printf("Produto: %d\n", produto);
+    printf("Quociente: %d\n", quociente);
+
+
+
+
+}
