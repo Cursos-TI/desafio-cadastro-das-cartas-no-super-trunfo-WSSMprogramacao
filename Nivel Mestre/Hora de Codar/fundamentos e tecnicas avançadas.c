0bcd34c67eb085426a180c3e44ffc30e7ed166db
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-18 21:31:56 +0000

    mestre

---
+++ Nivel Mestre/Hora de Codar/fundamentos e tecnicas avançadas.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main(){
+
+    //Declarar variaveis de produto, u i estoque, double valor unitario, double valor total e 
+    // u i quantidade minima
+    // exibir as informacoes do produto
+    //Comparaçoes do valor minimo do estoque
+    //Comparaçoes ds valores totais dos produtos
+
+    char produtoA[30]= "Produro A";
+    char produtoB[30]= "Produto B";
+
+    unsigned int estoqueA= 1000;
+    unsigned int estoqueB= 2000;
+
+    float valorA = 10.50;
+    float valorB = 20.40;
+    
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMinimoB = 2500;
+
+    double valorTotalA;
+    double valorTotalB;
+
+    int resultadoA, resultadoB;
+
+    //Exibir as informacoes do produto
+    printf("Produto %d tem em estoque %u e o valor unitario é %.2f\n" , produtoA, estoqueA, valorA);
+    printf("Produto %d tem em estoque %u e o valor unitario é %.2f\n" , produtoB, estoqueB, valorB); 
+    
+    //Comparacoes com valor minimo de estoque
+    resultado = estoqueA > estoqueMinimo;
+    resultado = estoqueB > estoqueMinimo;
+    
+    //Comparacoes com valores  totais de produtos
+    
+    
+
+
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
