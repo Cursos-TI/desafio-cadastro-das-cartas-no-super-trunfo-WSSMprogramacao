c193a5ea795e6132a6c1925e00f9c1e9d26d65ae
WSSMprogramacao <alunowanderlei77github@gmail.com>
2025-08-29 21:09:28 +0000

    tema 3

---
+++ TEMA_3_SUPER_TRUNFO.C/ESTRUT._DE_DECISAO_1.2.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    /*Se a 'idade' for 18 ou mais, o programa imprime ' Você é maior de idade'
+    No nosso exemplo , 20 e maior que 18, entao a mensagem sera exibida*/
+    
+    int idade = 20;
+
+    if (idade >= 18);{
+        printf(" Você é maior de idade\n");
+        printf(" Dentro if\n");
+    }
+
+
+
+
+
+        printf(" fora if\n");
+
+
+        return 0;
+
+
+
+
+
+
+
+}
